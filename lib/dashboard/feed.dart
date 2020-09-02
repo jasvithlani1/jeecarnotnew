6b5d55969e9b720058c5ebb53ce913b16bf57b7f
Rishabh Madan <rishabh4499@gmail.com>
2020-09-02 17:02:45 +0530

    fix feedback screen overflow

--- lib/dashboard/feed.dart
+++ lib/dashboard/feed.dart
@@ -39,58 +39,60 @@ class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              radius: 45,
-              backgroundColor: buttonColor,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: CircleAvatar(
-                radius: 40,
-                backgroundImage:
-                    AssetImage("assets/images/profilepicture1.jpg"),
+                radius: 45,
+                backgroundColor: buttonColor,
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage:
+                      AssetImage("assets/images/profilepicture1.jpg"),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Akash Mishra",
-              style: TextStyle(fontSize: 16, color: primaryColor),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Akash Mishra",
+                style: TextStyle(fontSize: 16, color: primaryColor),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 2,
-              width: MediaQuery.of(context).size.width / 1.25,
-              color: primaryColor,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 2,
+                width: MediaQuery.of(context).size.width / 1.25,
+                color: primaryColor,
+              ),
             ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 1.25,
-            child: PageView(
-              physics: ClampingScrollPhysics(),
-              controller: _pageController,
-              onPageChanged: (int page) {
-                setState(() {
-                  _currentPage = page;
-                });
-              },
-              children: [
-                FeedbackScreen(),
-                Feed1(),
-                Feed2(),
-              ],
+            Container(
+              height: MediaQuery.of(context).size.height / 1.25,
+              child: PageView(
+                physics: ClampingScrollPhysics(),
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                children: [
+                  FeedbackScreen(),
+                  Feed1(),
+                  Feed2(),
+                ],
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _buildPageIndicator(),
-          ),
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildPageIndicator(),
+            ),
+          ],
+        ),
       ),
     );
   }
